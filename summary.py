e5ae7c4cef5befa1d8f18ad5bec1dde4ccfbfff5
Alexandr Plakhotnikov <mxplaha@gmail.com>
2019-12-06 11:20:06 +0300

    chapter 4 add file summary

---
+++ summary.py
@@ -0,0 +1,46 @@
+datetime.today().minute
+os.getcwd()
+sys.platform
+sys.version
+os.environ
+os.getenv('HOME')
+datetime.date.isoformat(datetime.date.today())
+time.strftime("%I: %M")
+time.strftime("%A %p")
+html.escape("")
+html.unescape("")
+for i in range(4):
+time.sleep(5)
+dir(random)
+help(random.randint)
+random.randint(1, 60)
+for i in range(99, 0, -1):
+len()
+[list].append()
+if a not in []
+[list].remove()
+[list].pop
+[list].extend
+[list].insert(i, v)
+list()
+str = '',join(list)
+[list].copy
+'str' * 3
+for k in {dist}:
+  {dist}[k]
+sorted()
+for k, v in sorted({dist}.items()):
+{dist}.setdefault(k, v)
+set('aio')
+{many}.union{many}
+{many}.difference{many}
+{many}.intersection{many}
+type()
+("tuple",)
+pprint.pprint({dict})
+def function(a:srt, b:int) -> set:
+def function(a:srt, b:str='aeio') -> set:
+###
+def func(a:str, b:str='asdf') -> set:
+func(b='qwe', a='zxcv')
+###
