e9f6c6ecab51e0488778e15060ab27308a8ed417
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-02-20 13:44:55 +0300

    chapter 12 example  generator lists and expression-generator

---
+++ chapter_12/work_with_urls/expression_gen_urls.py
@@ -0,0 +1,6 @@
+import requests
+
+urls = ('http://yandex.ru', 'http://oreilly.com', 'http://twitter.com')
+
+for resp in (requests.get(url) for url in urls):
+    print(len(resp.content), '->', resp.status_code, '->', resp.url)
